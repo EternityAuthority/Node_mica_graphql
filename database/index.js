0e4c3aca629ee2880ca202029c4a4c385509b608
EternityAuthority <eternity.authority0513@gmail.com>
2021-10-05 17:34:24 +0900

    first commit

---
+++ database/index.js
@@ -0,0 +1,21 @@
+import DBConnect from "./db/DBConnect";
+import UserProvider from "./providers/UserProvider";
+import PostProvider from "./providers/adminProvider";
+import superAminProvider from "./providers/superAdminProvider";
+import productProvider from "./providers/productProvider";
+import orderProvider from "./providers/orderProvider";
+import adminProvider from "./providers/adminProvider";
+import combinationProvider from "./providers/combinationProvider"
+import brandFoundationProvider from "./providers/brandFoundationProvider"
+
+export {
+  DBConnect,
+  UserProvider,
+  PostProvider,
+  superAminProvider,
+  productProvider,
+  orderProvider,
+  adminProvider,
+  combinationProvider,
+  brandFoundationProvider
+};
